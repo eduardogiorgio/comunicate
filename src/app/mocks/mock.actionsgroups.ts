328aa14fbc1bbb9e457112e75380e39e25703966
eduardogiorgio <eduardo.giorgio69@gmail.com>
2019-04-29 23:10:16 -0300

    me olvide del mock

---
+++ src/app/mocks/mock.actionsgroups.ts
@@ -0,0 +1,35 @@
+import { ActionGroup } from '../models/action-group';
+
+export const ACTIONSGROUPS: ActionGroup[] = [
+    { idAction:4, idGroup: 1 },
+    { idAction:5, idGroup: 1 },
+    { idAction:6, idGroup: 1 },
+    { idAction:7, idGroup: 1 },
+
+    { idAction:8, idGroup: 2 },
+    { idAction:9, idGroup: 2 },
+
+    { idAction:10, idGroup: 3 },
+    { idAction:11, idGroup: 3 },
+    { idAction:12, idGroup: 3 },
+
+    { idAction:13, idGroup: 4 },
+    { idAction:14, idGroup: 4 },
+
+    { idAction:15, idGroup: 5 },
+
+    { idAction:16, idGroup: 6 },
+    { idAction:17, idGroup: 6 },
+    { idAction:18, idGroup: 6 },
+
+    { idAction:19, idGroup: 6 },
+    { idAction:20, idGroup: 6 },
+    { idAction:21, idGroup: 6 },
+
+    { idAction:22, idGroup: 7 },
+    { idAction:23, idGroup: 7 },
+    { idAction:24, idGroup: 7 },
+
+
+    
+];
