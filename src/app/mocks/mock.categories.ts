7b17c92fa86b30d958da96715b5371a05cfb7cdd
eduardogiorgio <eduardo.giorgio69@gmail.com>
2019-04-29 23:13:42 -0300

    fix bicicle

--- src/app/mocks/mock.categories.ts
+++ src/app/mocks/mock.categories.ts
@@ -3,7 +3,7 @@ import { Category } from '../models/category';
 export const CATEGORIES: Category[] = [
     { id: 1, name: 'Sentimientos', icon: 'happy'},
     { id: 2, name: 'Personas', icon: 'people'},
-    { id: 3, name: 'actividades', icon: 'bicycle"'},
+    { id: 3, name: 'actividades', icon: 'bicycle'},
     { id: 4, name: 'Lugares', icon: 'home'},
     { id: 5, name: 'Alimentos', icon: 'restaurant'}
 ];
