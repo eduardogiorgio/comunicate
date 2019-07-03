eb86c082a24ec041655043ed4e4c3c6e2d524f4e
eduardogiorgio <eduardo.giorgio69@gmail.com>
2019-07-02 22:14:55 -0300

    add_undo_all_actions

---
+++ src/app/models/restore-data.ts
@@ -0,0 +1,9 @@
+import { Category } from "./category";
+import { Group } from "./group";
+import { Action } from "./action";
+
+export class RestoreData {
+    categories: Category[];
+    groups: Group[];
+    actions: Action[];
+}
